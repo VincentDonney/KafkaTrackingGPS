e7cfc62a90c8e49ce9655b1ba4e1925c7e26802e
VincentDonney <donneyvinc@gmail.com>
2023-12-27 02:31:58 +0100

    marker's position is updated every second

--- trackingGPSfront/src/app/map/map.component.ts
+++ trackingGPSfront/src/app/map/map.component.ts
@@ -7,31 +7,41 @@ import { WebsocketService } from '../service/websocket.service';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent {
-  private map:any;
-  public number: string ='';
-  constructor(private websocket:WebsocketService){}
+export class MapComponent implements AfterViewInit {
+  private map: any;
+  public number: string = '';
+  public allowMoving: boolean = false;
 
-  ngAfterViewInit():void{
-    this.websocket.getNumber().subscribe((num: string)=>{
+  constructor(private websocket: WebsocketService) {}
+
+  ngAfterViewInit(): void {
+    this.websocket.getNumber().subscribe((num: string) => {
       this.number = num;
-    })
+    });
     this.initMap();
   }
 
   initMap(): void {
     this.map = L.map('map').setView([51.505, -0.09], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors'
+      attribution: '© OpenStreetMap contributors',
+      maxZoom: 18
     }).addTo(this.map);
 
     const pos1 = L.marker([51.5, -0.09]).addTo(this.map).bindPopup('Pos 1');
 
+    setInterval(() => {
+      if (this.allowMoving){
+      const currentLatLng = pos1.getLatLng();
+      pos1.setLatLng([currentLatLng.lat + 0.01, currentLatLng.lng]);
 
-    const pos2 = L.marker([51.5, -0.1]).addTo(this.map).bindPopup('Pos 2');
+      this.map.setView([currentLatLng.lat, currentLatLng.lng]);
+      }
+
+    }, 1000);
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.map.remove();
   }
 }
